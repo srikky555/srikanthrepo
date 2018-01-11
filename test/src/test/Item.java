f846c4f5eb6e7c3d5cde6eaa61d18ce6f86a9389
prokarma <prokarma@prokarma-PC>
2018-01-11 18:07:38 +0530

    my first brach creation

---
+++ test/src/test/Item.java
@@ -0,0 +1,21 @@
+package test;
+
+public class Item {
+	
+private	double  Price;
+private int Quantity ;
+public double getPrice() {
+	return Price;
+}
+public int getQuantity() {
+	return Quantity;
+}
+public void setPrice(double price) {
+	Price = price;
+}
+public void setQuantity(int quantity) {
+	Quantity = quantity;
+}
+
+
+}
